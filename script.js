23b9dbc397f764905edfabe34db38c59ef151c67
Pritish Dugar <pritishdugar509@gmail.com>
2021-11-12 15:19:35 +0530

    script code cleaned up

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoicHJpdGlzaDUwOSIsImEiOiJja3YzaTZzNzU0OGIxMzBzNzljdHdhb3g3In0.waaFZiEvtv7mzgcVhs5_Bw';
 const map = new mapboxgl.Map({
   container: 'map',
-  // Replace YOUR_STYLE_URL with your style URL.
   style: 'mapbox://styles/pritish509/ckvc0ze0uczk514p8ylwc7mnh',
 });
-
 
 map.getCanvas().style.cursor = 'default';
 map.fitBounds([
@@ -13,113 +11,6 @@ map.fitBounds([
 ]);
 
 
-// let hoveredStateId = null;
-
-// map.on('load', () => {
-//   map.addSource('provinces', {
-//     type: 'vector',
-//     // Use any Mapbox-hosted tileset using its tileset id.
-//     // Learn more about where to find a tileset id:
-//     // https://docs.mapbox.com/help/glossary/tileset-id/
-//     url: 'mapbox://pritish509.c55vhtbv'
-//   });
-
-//   map.addLayer({
-//     'id': 'province-fills',
-//     'type': 'fill',
-//     'source': 'provinces',
-//     'source-layer': 'afghanistan-provinces-bx5pi6',
-//     'layout': {},
-//     'paint': {
-//       'fill-color': '#627BC1',
-//       'fill-opacity': [
-//         'case',
-//         ['boolean', ['feature-state', 'hover'], false],
-//         1,
-//         0.5
-//       ]
-//     }
-//   });
-
-
-//   map.addLayer({
-//     'id': 'province-line',
-//     'type': 'line',
-//     'source': 'provinces',
-//     'source-layer': 'afghanistan-provinces-bx5pi6',
-//     'layout': {},
-//     'paint': {
-//       'line-color': 'black',
-//       'line-width': 1.5
-//     }
-//   });
-
-
-//   map.on('mousemove', 'province-fills', (e) => {
-//     const features = map.queryRenderedFeatures(e.point);
-
-//     // Limit the number of properties we're displaying for
-//     // legibility and performance
-//     const displayProperties = [
-//       'type',
-//       'properties',
-//       'id',
-//       'layer',
-//       'source',
-//       'sourceLayer',
-//       'state'
-//     ];
-
-//     hoveredStateId = e.features[0].id;
-//     map.setFeatureState( {source: 'provinces', sourceLayer: 'afghanistan-provinces-bx5pi6' , id: hoveredStateId}, { hover: true });
-
-//     const displayFeatures = features.map((feat) => {
-//       const displayFeat = {};
-//       displayProperties.forEach((prop) => {
-//         displayFeat[prop] = feat[prop];
-//       });
-//       return displayFeat;
-//     });
-    
-//     document.getElementById('features').innerText = JSON.stringify(displayFeatures[0]);
-//   });
-
-
-//   map.on('mousemove', 'state-fills', (e) => {
-//     if (e.features.length > 0) {
-//       if (hoveredStateId !== null) {
-//         map.setFeatureState(
-//           { source: 'states', 
-//           sourceLayer: 'afghanistan-provinces-bx5pi6', 
-//           id: hoveredStateId},
-//           { hover: false }
-//         );
-//       }
-//       hoveredStateId = e.features[0].id;
-//       map.setFeatureState(
-//         { source: 'states', 
-//           sourceLayer: 'afghanistan-provinces-bx5pi6', 
-//           id: hoveredStateId},
-//           { hover: true }
-//       );
-//     }
-//   });
-     
-//     // When the mouse leaves the state-fill layer, update the feature state of the
-//     // previously hovered feature.
-//   map.on('mouseleave', 'state-fills', () => {
-//     if (hoveredStateId !== null) {
-//       map.setFeatureState(
-//         { source: 'states', 
-//         sourceLayer: 'afghanistan-provinces-bx5pi6', 
-//         id: hoveredStateId},
-//         { hover: false }
-//       );
-//     }
-//     hoveredStateId = null;
-//   });
-  
-// });
 map.on('mousemove', (event) => {
   const features = map.queryRenderedFeatures(event.point, {
     layers: ['afghanistan-provinces-heat']
