e50803f3702ac3890d2c546827bc7f9440d38fc1
Pritish Dugar <pritishdugar509@gmail.com>
2021-11-14 15:16:50 +0530

    update

--- script.js
+++ script.js
@@ -4,29 +4,29 @@ const map = new mapboxgl.Map({
   style: 'mapbox://styles/pritish509/ckvc0ze0uczk514p8ylwc7mnh',
 });
 
-map.getCanvas().style.cursor = 'default';
-map.fitBounds([
-  [60.5284298033, 29.318572496],
+const afghanBoundsCoordinates = [
+  [62.5, 29.318572496],
   [75.1580277851, 38.4862816432]
-]);
+]
 
+map.getCanvas().style.cursor = 'default';
+map.fitBounds(afghanBoundsCoordinates);
 
-map.on('mousemove', (event) => {
-  const features = map.queryRenderedFeatures(event.point, {
-    layers: ['afghanistan-provinces-heat']
+map.on('load', () => {
+  map.on('mousemove', (event) => {
+    const features = map.queryRenderedFeatures(event.point, {
+      layers: ['afghanistan-provinces-heat']
+    });
+    const nameEl = document.getElementById('province-name')
+    const detailsEl = document.getElementById('province-details')
+    if (features.length) {
+      const layerProperties = features[0].properties
+      nameEl.innerHTML = `<h3>${layerProperties.NAME}</h3>`
+      detailsEl.innerHTML = `<p>Taliban Influence Level - ${layerProperties.Influence2009}</p>`
+    } else {
+      nameEl.innerHTML = '<p style="text-align: center; margin-top: 3.5em;">Touch or hover over province to view details</p>'
+      detailsEl.innerHTML = ''
+      map.fitBounds(afghanBoundsCoordinates);
+    }
   });
-  const nameEl = document.getElementById('province-name')
-  const detailsEl = document.getElementById('province-details')
-  if (features.length) {
-    const layerProperties = features[0].properties
-    nameEl.innerHTML = `<h3>${layerProperties.NAME}</h3>`
-    detailsEl.innerHTML = `<p>Taliban Influence Level - ${layerProperties.Influence2009}</p>`
-  } else {
-    nameEl.innerHTML = '<p style="text-align: center; margin-top: 3.5em;">Touch or hover over province to view details</p>'
-    detailsEl.innerHTML = ''
-    map.fitBounds([
-      [60.5284298033, 29.318572496],
-      [75.1580277851, 38.4862816432]
-    ]);
-  }
 });
