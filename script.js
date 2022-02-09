29c7dfaf1003a37c821d7dbc2aa4cf623727b731
Pritish Dugar <pritishdugar509@gmail.com>
2022-02-09 11:24:20 +0530

    prettier

--- script.js
+++ script.js
@@ -1,54 +1,42 @@
 // This token will not work for anyone else, please make your own token to access mapbox
-mapboxgl.accessToken = 'pk.eyJ1IjoicHJpdGlzaDUwOSIsImEiOiJja3YzaTZzNzU0OGIxMzBzNzljdHdhb3g3In0.waaFZiEvtv7mzgcVhs5_Bw';
+mapboxgl.accessToken =
+  "pk.eyJ1IjoicHJpdGlzaDUwOSIsImEiOiJja3YzaTZzNzU0OGIxMzBzNzljdHdhb3g3In0.waaFZiEvtv7mzgcVhs5_Bw";
 
 const map = new mapboxgl.Map({
-  container: 'map',
-  style: 'mapbox://styles/pritish509/ckvc0ze0uczk514p8ylwc7mnh',
+  container: "map",
+  style: "mapbox://styles/pritish509/ckvc0ze0uczk514p8ylwc7mnh",
 });
 
 const afghanBoundsCoordinates = [
   [58.5506, 27.062],
-  [80.5703, 39.3044]
-]
+  [80.5703, 39.3044],
+];
 
-const keyColors = [
-  '#840128',
-  '#e41b1e',
-  '#fdafaf',
-  '#ffffff'
-]
+const keyColors = ["#840128", "#e41b1e", "#fdafaf", "#ffffff"];
 
-const keyValues = [
-  'High',
-  'Moderate',
-  'Minimal',
-  'None'
-]
+const keyValues = ["High", "Moderate", "Minimal", "None"];
 
-const layerIds = [
-  'afghanistan-provinces-2009',
-  'afghanistan-provinces-2012'
-]
+const layerIds = ["afghanistan-provinces-2009", "afghanistan-provinces-2012"];
 
-map.getCanvas().style.cursor = 'default';
+map.getCanvas().style.cursor = "default";
 map.fitBounds(afghanBoundsCoordinates);
 
-map.on('load', () => {
+map.on("load", () => {
   // Generating legend details
-  const legend = document.getElementById('legend-items')
+  const legend = document.getElementById("legend-items");
 
   keyValues.forEach((value, i) => {
     const color = keyColors[i];
-    const item = document.createElement('div');
-    const key = document.createElement('span');
-    key.className = 'legend-key';
+    const item = document.createElement("div");
+    const key = document.createElement("span");
+    key.className = "legend-key";
     key.style.backgroundColor = color;
-    key.style.border = 'thin solid black'
-    key.style.display = 'inline-block'
-    key.style.margin = '0px 5px -2px 5px'
+    key.style.border = "thin solid black";
+    key.style.display = "inline-block";
+    key.style.margin = "0px 5px -2px 5px";
 
-    const info = document.createElement('span');
-    info.className = 'legend-info'
+    const info = document.createElement("span");
+    info.className = "legend-info";
     info.innerHTML = `${value}`;
 
     item.id = `${value}`;
@@ -57,72 +45,72 @@ map.on('load', () => {
     legend.appendChild(item);
   });
 
-
   // View province wise data
-  map.on('mousemove', (event) => {
+  map.on("mousemove", (event) => {
     // fetching the user chosen layer from buttons
-    const activeButton = document.getElementsByClassName('active')
-    const currentLayer = activeButton[0].getAttribute("id")
+    const activeButton = document.getElementsByClassName("active");
+    const currentLayer = activeButton[0].getAttribute("id");
 
     // Querying the features of the chosen layer
     const features = map.queryRenderedFeatures(event.point, {
-      layers: [currentLayer]
+      layers: [currentLayer],
     });
-    const nameEl = document.getElementById('province-name')
-    const detailsEl = document.getElementById('province-details')
+    const nameEl = document.getElementById("province-name");
+    const detailsEl = document.getElementById("province-details");
     if (features.length) {
-      const layerProperties = features[0].properties
-      const provinceName = layerProperties.NAME
+      const layerProperties = features[0].properties;
+      const provinceName = layerProperties.NAME;
       let talibanInfluence = null;
       switch (currentLayer) {
-        case 'afghanistan-provinces-2009':
+        case "afghanistan-provinces-2009":
           talibanInfluence = layerProperties.Influence2009;
           break;
-        case 'afghanistan-provinces-2012':
+        case "afghanistan-provinces-2012":
           talibanInfluence = layerProperties.Influence2012;
           break;
         default:
-          talibanInfluence = 'None';
+          talibanInfluence = "None";
       }
-      // Adding fetched layer details to the info table 
-      nameEl.innerHTML = `<h3>${provinceName}</h3>`
+      // Adding fetched layer details to the info table
+      nameEl.innerHTML = `<h3>${provinceName}</h3>`;
 
       // Highlighting legend value
       keyValues.forEach((value) => {
-        const legendItem = document.getElementById(`${value}`)
+        const legendItem = document.getElementById(`${value}`);
         if (value == talibanInfluence) {
-          $(`#${value}`).css("opacity","1")
-        } else $(`#${value}`).css("opacity","0.2")
-      })
+          $(`#${value}`).css("opacity", "1");
+        } else $(`#${value}`).css("opacity", "0.2");
+      });
     } else {
-      nameEl.innerHTML = '<p style="text-align: center; margin-top: 3.5em;">Touch or hover over province to view details</p>'
+      nameEl.innerHTML =
+        '<p style="text-align: center; margin-top: 3.5em;">Touch or hover over province to view details</p>';
       keyValues.forEach((value) => {
-        $(`#${value}`).css("opacity","1")
-      })
+        $(`#${value}`).css("opacity", "1");
+      });
       // ensuring that the map frame focuses on Afghanistan only
       map.fitBounds(afghanBoundsCoordinates);
     }
   });
 });
 
-// Runs when the last frame of the map has been rendered 
-map.on('idle', () => {
+// Runs when the last frame of the map has been rendered
+map.on("idle", () => {
   for (const id of layerIds) {
     // Skip layers that already have a button set up.
     if (document.getElementById(id)) {
       continue;
     }
     // Create a link which will act as a button.
-    const link = document.createElement('a');
+    const link = document.createElement("a");
     link.id = id;
-    link.href = '#';
+    link.href = "#";
     link.textContent = id.split("-")[2];
     // Adding the layer buttons to the button group div element
-    const layers = document.getElementById('layer-toggles');
+    const layers = document.getElementById("layer-toggles");
     layers.appendChild(link);
-    // Hard-coded, where the base layer from mapbox studio is toggled for button 
+    // Hard-coded, where the base layer from mapbox studio is toggled for button
     if (id == "afghanistan-provinces-2009") {
-      $(`#${id}`).addClass("active button")
+      $(`#${id}`).addClass("active button");
     } else $(`#${id}`).addClass("button");
 
     // Toggling layers logic for the created layer buttons
@@ -134,18 +122,14 @@ map.on('idle', () => {
       layerIds.forEach((id) => {
         // Toggle layer visibility by changing the layout object's visibility property.
         if (id === clickedLayer) {
-          map.setLayoutProperty(
-            clickedLayer,
-            'visibility',
-            'visible'
-          );
-          // Setting respective layer button class to active 
-          $(`#${id}`).addClass("active")
+          map.setLayoutProperty(clickedLayer, "visibility", "visible");
+          // Setting respective layer button class to active
+          $(`#${id}`).addClass("active");
         } else {
-          $(`#${id}`).removeClass("active")
-          map.setLayoutProperty(id, 'visibility', 'none');
+          $(`#${id}`).removeClass("active");
+          map.setLayoutProperty(id, "visibility", "none");
         }
-      })
+      });
     };
-  };
-})
+  }
+});
